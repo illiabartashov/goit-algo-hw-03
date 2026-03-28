8da6c27ecc7175593ebe361208987c57b2ec3041
Ilja <illabartasov@gmail.com>
2026-03-28 13:59:30 +0100

    First Task

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+#Завдання N°1
+from datetime import datetime
+
+def get_days_from_today(date_string):
+    try:
+        parsed_date = datetime.strptime(date_string, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Неправильний формат дати. Використовуйте 'YYYY-MM-DD'.")
+
+    today_date = datetime.today().date()
+    target_date = parsed_date.date()
+
+    difference = today_date - target_date
+
+    return difference.days
+
+print(get_days_from_today('2020-01-10'))
